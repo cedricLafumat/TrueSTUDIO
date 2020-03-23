94f0966871510141580a9f913749f0a277e999b1
cedricLafumat <cedricLafumat@le-campus-numerique.fr>
2020-03-23 11:21:26 +0100

    Ajout d'un projet led_blink pour le debut du codage C_embarqué

--- STM32_workspace_9.3/320_socket_server/src/server.h
+++ STM32_workspace_9.3/320_socket_server/src/server.h
@@ -7,7 +7,15 @@
 
 #ifndef SERVER_H_
 #define SERVER_H_
+#define LISTEN_PORT 5001
 
+int create_socket();
+
+int create_server();
+
+int listen_client(int socket_desc);
+
+int accept_connection();
 
 
 #endif /* SERVER_H_ */
