6f00e93eb2090812d6746f6efcd0fd24ae3aa521
cedricLafumat <cedricLafumat@le-campus-numerique.fr>
2020-11-16 13:22:54 +0100

    projet rtc qui fonctionne avec envoi des heures en debug vers le simulateur

---
+++ STM32_workspace_9.3/portage_p4_stm32_v2/Src/horodate.h
@@ -0,0 +1,24 @@
+/*
+ * horodate.h
+ *
+ *  Created on: 13 Nov 2020
+ *      Author: cedric
+ */
+
+#ifndef HORODATE_H_
+#define HORODATE_H_
+
+
+typedef struct{
+	int sec;
+	int min;
+	int hour;
+	int day;
+	int month;
+	int year;
+}Horodate_data;
+
+
+
+
+#endif /* HORODATE_H_ */
